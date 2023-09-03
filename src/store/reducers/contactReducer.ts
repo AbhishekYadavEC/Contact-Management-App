7580fdc29b765f03f8a4c389649d531075eabc09
Abhishek Yadav <86556735+AbhishekYadavEC@users.noreply.github.com>
2023-09-03 14:12:13 +0530

    contactReducer.ts

--- src/store/reducers/contactReducer.ts
+++ src/store/reducers/contactReducer.ts
@@ -1,4 +1,4 @@
-// store/reducers/contactReducer.ts
+
 import { createReducer } from '@reduxjs/toolkit';
 import { Contact, ContactActions } from '../actions/contactActions';
 
@@ -11,5 +11,4 @@ const initialState: ContactState = {
 };
 
 export const contactReducer = createReducer(initialState, {
-  // Implement your reducer logic for adding, editing, and deleting contacts here
 });
