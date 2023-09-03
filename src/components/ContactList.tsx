9038df23a6ba7d06649776a9961419ba3245c093
Abhishek Yadav <86556735+AbhishekYadavEC@users.noreply.github.com>
2023-09-03 14:09:54 +0530

    ContactList.tsx

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-
 const ContactList: React.FC = () => {
-  // Replace this with actual contact data and rendering logic
   const contacts = [
     { id: 1, name: 'John Doe', email: 'john@example.com' },
     { id: 2, name: 'Jane Smith', email: 'jane@example.com' },
   ];
-
   return (
     <div>
       <h2>Contact List</h2>
